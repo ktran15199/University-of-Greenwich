64e7138115dc0cd2568385322ddeaefd774c8e5a
ktran15199 <53217263+ktran15199@users.noreply.github.com>
2019-12-22 18:45:12 +0700

    Initial commit

---
+++ University of Greenwich/Controllers/RoleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using University_of_Greenwich.Models;
+
+namespace University_of_Greenwich.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext _context;
+        public RoleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Role
+        public ActionResult Index()
+        {
+            var Roles = _context.Roles.ToList();
+            return View(Roles);
+        }
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            _context.Roles.Add(Role);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+    
---
+++ University of Greenwich/Controllers/TraineesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using University_of_Greenwich.Models;
+
+namespace University_of_Greenwich.Controllers
+{
+    [Authorize]
+    public class TraineesController : Controller
+    {
+        private University_of_GreenwichEntities db = new University_of_GreenwichEntities();
+        [Authorize(Roles = "Staff,Trainee")]
+        // GET: Trainees
+        public ActionResult Index()
+        {
+            return View(db.Trainees.ToList());
+        }
+        [Authorize(Roles = "Staff,Trainee")]
+        // GET: Trainees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainee trainee = db.Trainees.Find(id);
+            if (trainee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainee);
+        }
+        [Authorize(Roles = "Staff")]
+        // GET: Trainees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trainees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TraineeID,TraineeName,TraineeDOB,TraineeAge,TraineeEducation,TraineeTOEIC")] Trainee trainee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trainees.Add(trainee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trainee);
+        }
+        [Authorize(Roles = "Staff")]
+        // GET: Trainees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainee trainee = db.Trainees.Find(id);
+            if (trainee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainee);
+        }
+
+        // POST: Trainees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TraineeID,TraineeName,TraineeDOB,TraineeAge,TraineeEducation,TraineeTOEIC")] Trainee trainee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trainee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trainee);
+        }
+        [Authorize(Roles = "Staff")]
+        // GET: Trainees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainee trainee = db.Trainees.Find(id);
+            if (trainee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainee);
+        }
+        [Authorize(Roles = "Staff")]
+        // POST: Trainees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trainee trainee = db.Trainees.Find(id);
+            db.Trainees.Remove(trainee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
